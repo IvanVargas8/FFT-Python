4700d10f087596f38c1568fb929b06b7aab0d466
Ivan Vargas <ivargaswhs88@berkeley.edu>
2018-02-28 18:06:26 -0800

    Colors program. Learning to connect pycharm and github

--- .ipynb_checkpoints/Colors.py
+++ .ipynb_checkpoints/Colors.py
@@ -1,4 +1,4 @@
-colors = ["red", "blue", "black","green"]
+colors = ["red", "blue", "black", "green"]
 i = 0
 while i < len(colors):
     print("When I was %d, my favorite color was %s" % (i, colors[i]))
